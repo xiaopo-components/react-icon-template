1267decca378b83b843ae1fd08bffb803553feec
xiaopo <wly19960911@qq.com>
2021-01-03 17:22:45 +0800

    添加生成结果

---
+++ lib/icons/done-all.tsx
@@ -0,0 +1,22 @@
+// THIS FILE CREATE BY build-tools/generate.js
+
+import React from "react";
+/**
+ *  if you want custom component name
+ *  you should modify template in 'build-tools/generate.js'
+ *
+ *  if you want custom your icon component, only modify 'lib/component/icon-component.tsx'
+ */
+import IconComponent, { IconProps } from "../component/icon-component";
+import Icon from "../assets/done-all.svg";
+
+const DoneAll: React.ForwardRefRenderFunction<HTMLSpanElement, IconProps> = (
+  props,
+  ref
+) => (
+  <IconComponent {...props} ref={ref}>
+    <Icon />
+  </IconComponent>
+);
+
+export default DoneAll;
